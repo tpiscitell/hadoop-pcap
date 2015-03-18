6aa2e1dacd489cdead20f3c3ec24e1adee814a09
Tom Piscitell <tpiscite@cisco.com>
2015-03-18 15:53:57 -0400

    working output format and record writer

--- hadoop-pcap-lib/src/main/java/net/ripe/hadoop/pcap/io/PcapOutputFormat.java
+++ hadoop-pcap-lib/src/main/java/net/ripe/hadoop/pcap/io/PcapOutputFormat.java
@@ -1,12 +1,22 @@
 package net.ripe.hadoop.pcap.io;
 
-import org.apache.hadoop.mapreduce.FileOutputFormat;
-import org.apache.hadoop.mapreduce.FileSystem;
-import org.apache.hadoop.mapreduce.Text;
-import org.apache.hadoop.mapreduce.MapWriteable;
-import org.apache.hadoop.mapreduce.JobConf;
-import org.apache.hadoop.mapreduce.Progresssable;
+import java.lang.String;
+import java.io.IOException;
 
+import org.apache.hadoop.io.BytesWritable;
+import org.apache.hadoop.io.ObjectWritable;
+import org.apache.hadoop.mapred.FileOutputFormat;
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.fs.FSDataOutputStream;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.io.MapWritable;
+import org.apache.hadoop.mapred.JobConf;
+import org.apache.hadoop.mapred.RecordWriter;
+import org.apache.hadoop.util.Progressable;
+
+import net.ripe.hadoop.pcap.io.writer.PcapRecordWriter;
+import net.ripe.hadoop.pcap.PcapReaderUtil;
 /**
  * Created by tpiscite on 3/17/15.
  */
@@ -19,27 +29,44 @@ public class PcapOutputFormat extends FileOutputFormat {
     public static final int SIG_FIGS = 0;
     public static final int SNAP_LEN =  65535;
     public static final int LINK_TYPE = 1; /* only support EN10MB for now */
+    public static final String REVERSE_HEADER_KEY = "net.ripe.hadoop.pcap.io.writer.reverseKey";
 
-    public RecordWriter<Text, MapWriteable> getRecordWriter(FileSystem ignored, JobConf job, String name, Progressable progress) {
+    public RecordWriter<Text, BytesWritable> getRecordWriter(FileSystem ignored, JobConf job, String name, Progressable progress)  throws IOException {
 
         Path file = FileOutputFormat.getTaskOutputPath(job, name);
         FileSystem fs = file.getFileSystem(job);
         FSDataOutputStream fileOut = fs.create(file, progress);
 
-        writePcapHeader(fileOut);
-        return new PcapRecordWriter<Text, MapWriteable>(fileOut);
+        writePcapHeader(fileOut, job.getBoolean(REVERSE_HEADER_KEY, true));
+        return new PcapRecordWriter(fileOut);
+    }
+
+    private byte[] intToByteArray(int d, boolean reverse) {
+        if (reverse) {
+            return new byte[] { (byte) d, (byte) (d >>> 8), (byte) (d >>> 16), (byte) (d >>> 24) };
+        } else {
+            return new byte[]{(byte) (d >>> 24), (byte) (d >>> 16), (byte) (d >>> 8), (byte) d};
+        }
+    }
+
+    private byte[] shortToByteArray(short s, boolean reverse) {
+        if (reverse) {
+            return new byte[] { (byte) s, (byte) (s >>> 8) };
+        } else {
+            return new byte[] { (byte) (s >>> 8), (byte) s };
+        }
     }
 
     /**
      * Write the Libpcap file header. See: https://wiki.wireshark.org/Development/LibpcapFileFormat
      */
-    private void writePcapHeader(DataOutputStream out) {
-        out.write(MAGIC_NUMBER);
-        out.write(MAJOR_VERSION);
-        out.write(MINOR_VERSION);
-        out.write(THIS_ZONE);
-        out.write(SIG_FIGS);
-        out.write(SNAP_LEN);
-        out.write(LINK_TYPE);
+    private void writePcapHeader(FSDataOutputStream out, boolean reverse) throws IOException {
+        out.write(intToByteArray(MAGIC_NUMBER, reverse));
+        out.write(shortToByteArray(MAJOR_VERSION, reverse));
+        out.write(shortToByteArray(MINOR_VERSION, reverse));
+        out.write(intToByteArray(THIS_ZONE, reverse));
+        out.write(intToByteArray(SIG_FIGS, reverse));
+        out.write(intToByteArray(SNAP_LEN, reverse));
+        out.write(intToByteArray(LINK_TYPE, reverse));
     }
 }
--- hadoop-pcap-lib/src/main/java/net/ripe/hadoop/pcap/io/writer/PcapRecordWriter.java
+++ hadoop-pcap-lib/src/main/java/net/ripe/hadoop/pcap/io/writer/PcapRecordWriter.java
@@ -1,30 +1,40 @@
 package net.ripe.hadoop.pcap.io.writer;
 
 import net.ripe.hadoop.pcap.packet.DataPacket;
+import net.ripe.hadoop.pcap.packet.Packet;
 
-import org.apache.hadoop.mapreduce.RecordWriter;
+import org.apache.hadoop.io.BytesWritable;
+import org.apache.hadoop.io.ObjectWritable;
 import org.apache.hadoop.io.Text;
-import org.apache.hadoop.mapreduce.MapWriteable;
+import org.apache.hadoop.mapred.RecordWriter;
+import org.apache.hadoop.mapred.Reporter;
+import org.apache.hadoop.fs.FSDataOutputStream;
 
-import java.io.DataOutputStream;
+import java.io.IOException;
+
 
 /**
  * Created by tpiscite on 3/17/15.
  */
-public class PcapRecordWriter<Text, MapWriteable> implements RecordWriter<Text, MapWriteable> {
+public class PcapRecordWriter implements RecordWriter<Text, BytesWritable> {
 
-    PcapRecordWriter (DataOutputStream out) {
+    FSDataOutputStream out;
+
+    public PcapRecordWriter (FSDataOutputStream out) {
         this.out = out;
     }
 
-    private void writePacket(Packet packet) {
-        out.write(packet.get(DataPacket.PACKET_HEADER));
-        out.write(packet.get(DataPacket.PACKET_HEADER));
+    public void write(Text key, BytesWritable value) throws IOException {
+        /**
+         * getBytes() returns the backing byte array which is padded. See:
+         * https://issues.apache.org/jira/browse/HADOOP-6298
+         */
+        byte[] packetData = new byte[value.getLength()];
+        System.arraycopy(value.getBytes(), 0, packetData, 0, value.getLength());
+        out.write(packetData);
     }
-    public void write(Text key, MapWriteable value) {
-        Packet packet = (Packet) value;
-        if (packet.containsKey(DataPacket.PACKET_HEADER) && packet.containsKey(DataPacket.PACKET_DATA)) {
-            writePacket(packet);
-        }
+
+    public void close(Reporter reporter) throws IOException {
+        out.close();
     }
 }
